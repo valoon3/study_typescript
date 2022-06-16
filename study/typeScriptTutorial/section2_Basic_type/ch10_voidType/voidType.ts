92d5d6c194dac1360dc86c5e3b3ee1983d601c86
valoon3 <valoon2@naver.com>
2022-06-16 22:51:22 +0900

    section2 1. voidType

---
+++ study/typeScriptTutorial/section2_Basic_type/ch10_voidType/voidType.ts
@@ -0,0 +1,8 @@
+// return 값이 void임을 나타냄
+function log(message): void {
+//    console.log(messsage);
+}
+
+let useless: void = undefined;
+//useless = 1; // error
+// useless 에서 에러가 나는 이유는 null은 되지만 undefined 는 허용되지 않는다.
